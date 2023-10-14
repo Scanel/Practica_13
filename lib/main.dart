806215180648879c62ea75e4e72cf7dc583de85a
César Fierro <79662848+Scanel@users.noreply.github.com>
2023-10-14 12:17:29 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:practica_14/src/crud_page.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+Future main() async {
+  sqfliteFfiInit();
+  databaseFactory = databaseFactoryFfi;
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: CrudPage(),
+    );
+  }
+}
